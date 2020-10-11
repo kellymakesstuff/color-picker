e6ae4b418ca43007df14fd3e1e25d376871c1ce1
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-10-11 18:02:31 -0400

    family and color models

---
+++ Models/Color.cs
@@ -0,0 +1,16 @@
+namespace ColorPicker.Models
+{
+  public class Color
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string HexCode { get; set; }
+    public string RGBCode { get; set; }
+    public string ComplimentColor { get; set; }
+
+
+    public int FamilyId { get; set; }
+    public Family Family { get; set; }
+
+  }
+}
---
+++ Models/Family.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace ColorPicker.Models
+{
+  public class Family
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Compliment { get; set; }
+
+    public IList<Color> Colors { get; set; } = new List<Color>();
+
+  }
