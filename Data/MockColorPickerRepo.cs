cd466bd2fcaaaa103fe2793ddc180574a00a5073
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-10-11 18:15:27 -0400

    implementations added

--- Data/MockColorPickerRepo.cs
+++ Data/MockColorPickerRepo.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+using ColorPicker.Models;
+
 namespace ColorPicker.Data
 {
   public class MockColorPickerRepo : IColorPickerRepo
   {
+    public IEnumerable<Family> GetAllFamilies()
+    {
+      throw new System.NotImplementedException();
+    }
 
+    public Family getFamilyById(int id)
+    {
+      throw new System.NotImplementedException();
+    }
   }
 }
