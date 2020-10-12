88eedf02c83cce4aeb8dfdf5ac6d4a9d41a7a855
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-10-12 11:57:46 -0400

    FamilyReadDto added

---
+++ Dtos/FamilyReadDto.cs
@@ -0,0 +1,13 @@
+namespace ColorPicker.Dtos
+{
+  public class FamilyReadDto
+  {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Type { get; set; }
+
+    public string Compliment { get; set; }
+  }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ColorPicker.Data;
-using ColorPicker.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using System;
 
 namespace ColorPicker
 {
@@ -34,9 +28,9 @@ namespace ColorPicker
 
       services.AddControllers();
 
-      services.AddScoped<IColorPickerRepo, SqlColorPickerRepo>();
-      // services.AddScoped<IColorPickerRepo, MockColorPickerRepo>();
+      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+      services.AddScoped<IColorPickerRepo, SqlColorPickerRepo>();
 
     }
 
