46b0c079b8bdb4c79905c6597ac48383a4c56792
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-10-11 20:28:46 -0400

    added dbcontext

---
+++ Data/ColorPickerContext.cs
@@ -0,0 +1,15 @@
+using ColorPicker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ColorPicker.data
+{
+  public class ColorPickerContext : DbContext
+  {
+    public ColorPickerContext(DbContextOptions<ColorPickerContext> opt) : base(opt)
+    {
+
+    }
+
+    public DbSet<Family> Families { get; set; }
+  }
+}
